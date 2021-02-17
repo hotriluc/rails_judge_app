601cd8ce03d2b74ec12337f5942378c0fe9bca73
Luc Ho <hotriluc97@gmail.com>
2021-02-17 18:22:17 +0200

    Redirect to user after signup or sign in

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,11 +50,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
 
-  # # The path used after sign up.
-  # def after_sign_up_path_for(user)
-  #   puts 'hiasidais'
-  #   user_path(current_user.id)
-  # end
+  # The path used after sign up.
+  def after_sign_up_path_for(resource)
+    puts 'hiasidais'
+    user_path(current_user)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
