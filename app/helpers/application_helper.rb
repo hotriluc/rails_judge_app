89232448593adbbf34543a82424c84f66d1953d4
hotriluc <hotriluc97@gmail.com>
2021-02-22 15:41:33 +0200

    Add login form on home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,20 @@ module ApplicationHelper
   end
 
 
+  #3 methods bellow
+  # to get access to resource(user) when add devise login form on home page
+  def resource_name
+      :user
+  end
+
+  def resource
+      @resource ||= User.new
+  end
+
+  def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+  end
+
+
+
 end
